8f9b097fbf45274f7d7bab83aaad57146978952d
abhishekpatel10 <abhishekpatel2423@gmail.com>
2025-07-28 17:38:29 -0500

    Final changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: process.env.baseURL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
-    req.headers.Authorization = `Bearer ${
-      JSON.parse(localStorage.getItem("profile")).token
-    }`;
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token
+      }`;
   }
 
   return req;
@@ -15,8 +14,7 @@ export const fetchPost = (id) => API.get(`/posts/${id}`);
 export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
 export const fetchPostsBySearch = (searchQuery) =>
   API.get(
-    `/posts/search?searchQuery=${searchQuery.search || "none"}&tags=${
-      searchQuery.tags
+    `/posts/search?searchQuery=${searchQuery.search || "none"}&tags=${searchQuery.tags
     }`
   );
 export const createPost = (newPost) => API.post("/posts", newPost);
